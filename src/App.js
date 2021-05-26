9b14da75a8b601fb585f01bbf230ab7371f93fa8
chavindu_weerakoon <chavindu@randomsoftware.net>
2021-05-26 23:05:36 +0530

    imported compnents to app.js through index.js (import all compnents through this )

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+//import components
+// import Cards from "./components/Cards/Cards";
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+import { Cards, chart, CountryPicker } from "./components";
+
 class App extends React.Component {
   render() {
     return (
