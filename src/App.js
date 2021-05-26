ae1b79e80ae451eb0bc9ea3e6b20e63769bccbbd
chavindu_weerakoon <chavindu@randomsoftware.net>
2021-05-26 23:18:34 +0530

    Added the basic project structure and css implementaion

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import React from "react";
 // import Chart from "./components/Chart/Chart";
 // import CountryPicker from "./components/CountryPicker/CountryPicker";
 
-import { Cards, chart, CountryPicker } from "./components";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards></Cards>
+        <CountryPicker></CountryPicker>
+        <Chart></Chart>
       </div>
     );
   }
