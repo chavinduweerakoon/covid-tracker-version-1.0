a08e6a0385663553f1c24f12bf094bf74ac4d897
chavindu_weerakoon <chavindu@randomsoftware.net>
2021-06-03 16:30:02 +0530

    Added the basic cards with material ui

--- src/App.js
+++ src/App.js
@@ -10,15 +10,20 @@ import styles from "./App.module.css";
 import { fetchData } from "./api";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
   async componentDidMount() {
-    const data = await fetchData();
+    const fetchedData = await fetchData();
 
-    console.log(data);
+    this.setState({ data: fetchedData });
   }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards></Cards>
+        <Cards data={data}></Cards>
         <CountryPicker></CountryPicker>
         <Chart></Chart>
       </div>
